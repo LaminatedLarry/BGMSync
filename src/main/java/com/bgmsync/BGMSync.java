c91db73b11aafe2ae02f8275d03ae8f013671253
LaminatedLarry <lawrence.emerine@gmail.com>
2025-10-27 17:56:13 -0400

    Update BGMSync.java

--- src/main/java/com/bgmsync/BGMSync.java
+++ src/main/java/com/bgmsync/BGMSync.java
@@ -78,7 +78,8 @@ public class BGMSync implements ModInitializer {
                             return 1;
                         }
                         ServerPlayNetworking.send(dj, BGMSyncPayloads.Test.INSTANCE);
-                        ctx.getSource().sendFeedback(() -> Text.literal("[BGMSync] Test triggered for DJ: " + dj.getName().getString()), true);
+                        final String __djName = dj.getName().getString(); // make effectively final for the supplier
+                        ctx.getSource().sendFeedback(() -> Text.literal("[BGMSync] Test triggered for DJ: " + __djName), true);
                         return 1;
                     }))
                     // /bgmsync set <player>
@@ -88,8 +89,9 @@ public class BGMSync implements ModInitializer {
                                 MinecraftServer server = ctx.getSource().getServer();
                                 ServerPlayerEntity target = EntityArgumentType.getPlayer(ctx, "player");
                                 setDJ(server, target);
+                                final String __tName = target.getName().getString();
                                 ctx.getSource().sendFeedback(
-                                    () -> Text.literal("[BGMSync] DJ set to: " + target.getName().getString()),
+                                    () -> Text.literal("[BGMSync] DJ set to: " + __tName),
                                     true
                                 );
                                 // If a track is active, re-broadcast so listeners follow new DJ's state
@@ -109,8 +111,9 @@ public class BGMSync implements ModInitializer {
                         if (dj == null) {
                             ctx.getSource().sendFeedback(() -> Text.literal("[BGMSync] No DJ is currently set."), false);
                         } else {
+                            final String __djName = dj.getName().getString();
                             ctx.getSource().sendFeedback(
-                                () -> Text.literal("[BGMSync] Current DJ: " + dj.getName().getString()),
+                                () -> Text.literal("[BGMSync] Current DJ: " + __djName),
                                 false
                             );
                         }
